47612777eb130494901af86a71128698964375eb
uzebciukaite <urtezeb@gmail.com>
2022-09-28 12:59:29 +0300

    photoslider

--- src/components/PhotoSlider.js
+++ src/components/PhotoSlider.js
@@ -23,20 +23,18 @@ const PhotoSlider = ({photos}) => {
   return (
     <Container className='flex-center w-100'>
         {photos.length >= 2 ? (
-        <>
+        
+        <div 
+        className='d-flex align-items-center justify-content-between w-75 profilePhoto'
+        style={{ backgroundImage: 'url(' + photos[photoIndex] + ')' }}>
         <div onClick={() => setPhotoIndex(photoIndex - 1) } className='slideIcons flex-center'>
             <MdNavigateBefore className='fs-2 text-light'/>
-        </div>
-        <div 
-        className='w-75'
-        >
-                <img className='profilePhoto rounded' src={photos[photoIndex]} alt="" />
         </div>
         <div onClick={() => setPhotoIndex(photoIndex + 1) } className='slideIcons  flex-center text-light'>
             <MdNavigateNext className='fs-2 text-light'/>
         </div>
-            
-        </>
+        </div>
+       
         ) : 
         <div className='w-75'>
             <img className='profilePhoto rounded' src="https://odenta32.lt/wp-content/uploads/2022/05/blank-profile-picture-973460_640.png" alt="" />
